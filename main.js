f48a0e1ceb8f9cd1cf610f2dbe087dad68d68344
rainchiu <welly81211@gmail.com>
2015-05-24 18:41:21 +0800

    firebase data

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
+var myDataRef;
 var App = React.createClass({
   getInitialState: function() {
-    return {data: [], text: ''};
+    return {data: [], tmptext: ''};
+  },
+  componentWillMount: function(){
+    myDataRef = new Firebase('https://rainchiu.firebaseio.com/');
+    var _self = this;
+    myDataRef.on('child_added', function(snapshot) {
+      var message = snapshot.val();
+      _self.state.data.push(message.text);
+      _self.setState({data: _self.state.data});     
+    });
+  },
+  componentDidMount: function(){
+  },
+  componentWillUnmount: function(){
+    myDataRef.off();
   },
   onChange: function(e) {
-    this.setState({text: e.target.value});
+    this.setState({tmptext: e.target.value});
   },
   addTodo: function(e) {
     e.preventDefault();
-    this.state.data.push(this.state.text);
-    localStorage.data = JSON.stringify(this.state.data);
-    this.setState({data: this.state.data, text: ''});
+    // this.state.data.push(this.state.tmptext);
+    myDataRef.push({text: this.state.tmptext});
+    this.setState({data: this.state.data, tmptext: ''});
+    console.log(this.state.data);
   },
   removeTodo: function(index) {
     this.state.data.splice(index, 1);
-    localStorage.data = JSON.stringify(this.state.data);
+    console.log(myDataRef.key());
+    myDataRef.remove();
     this.setState({data: this.state.data});
+    this.firebasedata();
   },
   updateTodo: function(index, value) {
     this.state.data.splice(index, 1, value);
-    localStorage.data = JSON.stringify(this.state.data);
     this.setState({data: this.state.data});
-    // console.log(this.state.data);
+    this.firebasedata();
+  },
+  firebasedata: function(){ 
+    
   },
   render: function() {
     return (
       <div>
         <form onSubmit={this.addTodo}>
-          <input className="myTxt" onChange={this.onChange} value={this.state.text} />
+          <input className="myTxt" onChange={this.onChange} value={this.state.tmptext} />
           <button className="btn btn-default btn-sm pull-right">ADD</button>
         </form>
         <TodoList data={this.state.data} onClickOk={this.updateTodo} onClickClose={this.removeTodo}/>
@@ -39,15 +59,8 @@ var TodoList = React.createClass({
     return {
             editdata: this.props.data,
             onEdit:[]
-           };
-    
+           };  
   },
-  // componentWillMount: function(){
-  //   console.log("componentWillMount");
-  // },
-  // componentDidMount: function(){
-  //   console.log("componentDidMount");
-  // },
   onChange: function(e) {
     this.state.editdata.splice(e.target.getAttribute('data-index'), 1, e.target.value);
     this.setState({editdata: this.state.editdata});
@@ -61,16 +74,22 @@ var TodoList = React.createClass({
       } 
     }
     this.setState({onEdit:this.state.onEdit});
-    console.log(this.state.onEdit);
+    // console.log(this.state.onEdit);
   },
-  editHandler: function(index){
-    this.state.onEdit.push(index);
+  editHandler: function(e){
+    this.state.onEdit.push(parseInt(e.target.getAttribute('data-index')));
     this.setState({onEdit:this.state.onEdit});
-    console.log(this.state.onEdit);
+    // console.log(this.state.onEdit);
   },
   deletedata: function(e){
     this.props.onClickClose(e.target.getAttribute('data-index'));
-    console.log(this.state.onEdit);
+    for(i = 0; i< this.state.onEdit.length ; i++){
+      if(this.state.onEdit[i]>=e.target.getAttribute('data-index')){
+         this.state.onEdit[i]-=1;
+      } 
+    }
+    this.setState({editdata:this.props.data});
+    // console.log(this.state.onEdit);
   },
   render: function() {
     var _self = this;
@@ -84,7 +103,7 @@ var TodoList = React.createClass({
           <div className="display">
             <span className="input-group-addon fontsize">{itemText}</span>
             <button className="btn btn-primary pull-right btn-sm removebtn" onClick={_self.deletedata} data-index={index}>remove</button>
-            <button className="btn btn-info pull-right btn-sm editbtn" onClick={_self.editHandler.bind(this, index)} data-index={index}>edit</button>
+            <button className="btn btn-info pull-right btn-sm editbtn" onClick={_self.editHandler} data-index={index}>edit</button>
           </div>  
         </li>       
       );
